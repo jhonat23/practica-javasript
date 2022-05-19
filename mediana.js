f964340c609ba628ce202ac2013a0425d55af642
jhonat23 <jhonat23@gmail.com>
2022-05-18 21:18:14 -0500

    new classes

--- mediana.js
+++ mediana.js
@@ -1,21 +1,32 @@
-const lista1 = [
+const lista = [
     100,
     200,
     500,
     40000000,
 ]
 
-const mitad1 = lista1.length / 2;
-const mitad2 = mitad1 + 1;
+function calculoMediana(lista1){
+    //Ordenando la lista
+    lista1.sort(function(a ,b){
+        return a - b;
+    })
 
-if (lista1.length % 2 === 0){
-    //tomar datos de la mitad y sacar promedio
-    let promedio = (lista1[mitad1 - 1] + lista1[mitad2 - 1]) / 2;
-    console.log("la mediana es: " + promedio);
-    console.log("esta es una lista con elementos pares");
-}else{
-    //tomar e imprimir dato de la mitad
-    let mediana = lista1[parseInt(mitad2 - 1)];
-    console.log("la mediana es: " + mediana);
-    console.log("esta es una lista con elementos impares");
+    console.log(lista1);
+
+    const mitad1 = lista1.length / 2;
+    const mitad2 = mitad1 + 1;
+
+    if (lista1.length % 2 === 0){
+        //tomar datos de la mitad y sacar promedio
+        let mediana = (lista1[mitad1 - 1] + lista1[mitad2 - 1]) / 2;
+        console.log("la mediana es: " + mediana);
+        console.log("esta es una lista con elementos pares");
+        return mediana;
+    }else{
+        //tomar e imprimir dato de la mitad
+        let mediana = lista1[parseInt(mitad2 - 1)];
+        console.log("la mediana es: " + mediana);
+        console.log("esta es una lista con elementos impares");
+        return mediana;
+}
 }
